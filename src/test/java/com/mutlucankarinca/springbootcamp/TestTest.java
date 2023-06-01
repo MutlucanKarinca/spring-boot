de236bf255305477c6a5e26bcf2d314a30622b63
Mutlucan <mutlucankarinca@gmail.com>
2023-06-01 22:17:09 +0300

    add TestTest

---
+++ src/test/java/com/mutlucankarinca/springbootcamp/TestTest.java
@@ -0,0 +1,41 @@
+package com.mutlucankarinca.springbootcamp;
+
+import org.junit.jupiter.api.*;
+
+public class TestTest {
+
+    @BeforeAll
+    static void setup(){
+        System.out.println("beforeAll");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("afterAll");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("beforeEach");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("afterEach");
+    }
+
+    @Test
+    void test1(){
+        System.out.println("test1");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("test2");
+    }
+
+    @Test
+    void test3(){
+        System.out.println("test3");
+    }
+}
